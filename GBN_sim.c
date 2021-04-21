22c22a448c434ac56d26ceec8ed5b98ae7465dee
jmitch6 <jmitch6@masonlive.gmu.edu>
2021-04-21 14:32:59 -0400

    asdf

--- GBN_sim.c
+++ GBN_sim.c
@@ -154,13 +154,8 @@ void A_input(packet)
   {
       printf("    A_input checksum passed, ACK recieved for %d\n", packet.acknum);
   }
-  if (packet.acknum != A.lastACK)
-  {
-      printf("    A_input not correct sequence number. Expected %d, got %d\n", A.next_seq, packet.acknum);
-      return;
-  }
   //add the new packet
-  A.lastACK = packet.acknum;
+  A.lastACK = packet.acknum +1;
   //check if no more packets in buffer
   if (A.lastACK == A.next_seq)
   {
